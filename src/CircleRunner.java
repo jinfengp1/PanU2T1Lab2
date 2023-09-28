55d8213ad242e27b0345a006bb27d19d1fa8b93d
Jin Feng Pan <jinfengp@nycstudents.net>
2023-09-28 11:27:33 -0400

    Initial commit

---
+++ src/CircleRunner.java
@@ -0,0 +1,15 @@
+public class CircleRunner {
+
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+Circle c1 = new Circle(5.0);
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+c1.printArea();
+        // C. BEFORE running this code, PREDICT what gets printed
+//whatever 3.14 x 25 is (and its a double value btw)
+        // D. run the code and confirm your prediction! Were you correct?
+        // yes
+    }
+
+}
